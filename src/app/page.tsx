a922e610711e2673680f048c1383699c8af5e8a5
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-23 15:41:58 +0800

    feat: restructure Home component layout to improve header positioning and body spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ import Body from "@/components/forms/body";
 
 export default function Home() {
   return (
-  <div className="mx-8">
-    <Header />
-    <Separator />
-    <Body />
+  <div>
+    <div className="fixed top-0 left-0 right-0 z-50 bg-background">
+      <div className="mx-8">
+        <Header />
+        <Separator />
+      </div>
+    </div>
+    <div className="mx-8 pt-20">
+      <Body />
+    </div>
   </div>
   );
 }

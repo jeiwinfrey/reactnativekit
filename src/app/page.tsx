6f074bde10c0574749123a811ef8cdabf85929fa
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-23 15:14:36 +0800

    feat: integrate Body component into Home layout for enhanced content display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/forms/header";
 import { Separator } from "@/components/ui/separator";
+import Body from "@/components/forms/body";
 
 export default function Home() {
   return (
   <div className="mx-8">
     <Header />
     <Separator />
+    <Body />
   </div>
   );
 }

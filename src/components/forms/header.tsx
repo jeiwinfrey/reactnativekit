3830469920eb4baae6ec046a154182533e3a1456
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-23 13:58:03 +0800

    feat: update Header component to include external links for social media buttons

--- src/components/forms/header.tsx
+++ src/components/forms/header.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { TwitterIcon } from "@/assets/icons/twitter";
 import { RNKitIcon } from "@/assets/icons/rnkit";
 import { MailIcon } from "@/assets/icons/mail";
 
 export default function Header() {
+
+
   return (
     <div className="flex justify-between items-center p-4">
       <div className="flex items-center gap-2">
@@ -12,11 +16,15 @@ export default function Header() {
       </div>
 
       <div className="flex items-center space-x-2">
-        <Button variant="ghost" size="icon">
-          <TwitterIcon className="h-4 w-4" />
+        <Button variant="ghost" size="icon" asChild>
+          <a href="https://x.com/jeiwinfrey" target="_blank" rel="noopener noreferrer">
+            <TwitterIcon className="h-4 w-4" />
+          </a>
         </Button>
-        <Button variant="ghost" size="icon">
-          <MailIcon className="h-4 w-4" />
+        <Button variant="ghost" size="icon" asChild>
+          <a href="mailto:jeiwinfreyulep@outlook.com">
+            <MailIcon className="h-4 w-4" />
+          </a>
         </Button>
       </div>
     </div>

3aedf4852e37a33e8855657becc722862e4d6cc4
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-24 11:20:11 +0800

    feat: integrate Tabs component into Header for navigation

--- src/components/forms/header.tsx
+++ src/components/forms/header.tsx
@@ -4,6 +4,9 @@ import { Button } from "@/components/ui/button";
 import { TwitterIcon } from "@/assets/icons/twitter";
 import { RNKitIcon } from "@/assets/icons/rnkit";
 import { MailIcon } from "@/assets/icons/mail";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { HugeiconsSmartPhone02 } from "@/assets/icons/phone";
+import { HugeiconsFullScreen } from "@/assets/icons/boilerplate";
 
 export default function Header() {
 
@@ -13,6 +16,18 @@ export default function Header() {
       <div className="flex items-center gap-2">
         <RNKitIcon className="h-4 w-4" />
         <div> React Native Kit </div>
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="home">
+              <HugeiconsSmartPhone02 className="h-4 w-4" />
+              Screens
+            </TabsTrigger>
+            <TabsTrigger value="boilerplates">
+              <HugeiconsFullScreen className="h-4 w-4" />
+              Boiler Plates
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
       </div>
 
       <div className="flex items-center space-x-2">
